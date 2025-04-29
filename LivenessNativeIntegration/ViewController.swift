a20c54644ca76c365e527322e706562e6ddc6539
@algoreadme <dhika.aditya@kpvendor.id>
2025-04-29 13:31:33 +0700

    Initial Commit

---
+++ LivenessNativeIntegration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LivenessNativeIntegration
+//
+//  Created by @algoreadme on 29/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
