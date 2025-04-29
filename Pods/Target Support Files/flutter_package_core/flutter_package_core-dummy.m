e3435777c1a7c4c95d8307934308896b05ff227f
@algoreadme <dhika.aditya@kpvendor.id>
2025-04-29 16:48:46 +0700

    feat: create and implement liveness gateway

---
+++ Pods/Target Support Files/flutter_package_core/flutter_package_core-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_flutter_package_core : NSObject
+@end
+@implementation PodsDummy_flutter_package_core
+@end
---
+++ Pods/Target Support Files/flutter_package_plugin_liveness/flutter_package_plugin_liveness-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_flutter_package_plugin_liveness : NSObject
+@end
+@implementation PodsDummy_flutter_package_plugin_liveness
+@end
