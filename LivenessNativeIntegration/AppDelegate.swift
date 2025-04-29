ffe5eac457d104111a4461d4878919d4166cda30
@algoreadme <dhika.aditya@kpvendor.id>
2025-04-29 16:41:50 +0700

    feat: create and implement liveness gateway

--- LivenessNativeIntegration/AppDelegate.swift
+++ LivenessNativeIntegration/AppDelegate.swift
@@ -6,31 +6,35 @@
 //
 
 import UIKit
+import Flutter
+// The following library connects plugins with iOS platform code to this app.
+import FlutterPluginRegistrant
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+@UIApplicationMain
+class AppDelegate: FlutterAppDelegate { // More on the FlutterAppDelegate.
+    lazy var flutterEngine = FlutterEngine(name: "my flutter engine")
+    
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Runs the default Dart entrypoint with a default Flutter route.
+        flutterEngine.run();
+        // Connects plugins with iOS platform code to this app.
+        GeneratedPluginRegistrant.register(with: self.flutterEngine);
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions);
     }
-
+    
+    /*
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    */
 }
 
